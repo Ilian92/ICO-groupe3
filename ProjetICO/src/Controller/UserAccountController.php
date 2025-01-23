32e916249f188755114fbb43def7c245d3ea0dc6
Noan <delatouchenoan@gmail.com>
2025-01-23 22:42:08 +0100

    applied css to user crud

--- ProjetICO/src/Controller/UserAccountController.php
+++ ProjetICO/src/Controller/UserAccountController.php
@@ -1,10 +1,14 @@
 <?php
-// filepath: /C:/Users/morga/OneDrive/Documents/EEMI/3ème année/-- WORKSHOP ICO/ICO-groupe3/ProjetICO/src/Controller/UserAccountController.php
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserAccountController extends AbstractController
 {
@@ -24,5 +28,22 @@ class UserAccountController extends AbstractController
     public function orderHistory(): Response
     {
         return $this->render('user_account/order_history.html.twig');
+    }
+
+    #[Route('/compte/modifier', name: 'user_account_edit', methods: ['GET', 'POST'])]
+    public function editProfile(Request $request, EntityManagerInterface $entityManager, UserInterface $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_account');
+        }
+
+        return $this->render('user_account/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
