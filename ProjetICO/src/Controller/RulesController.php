bdd059b50fab105bbd9060e6dc618ba559d1282b
Dassonville Morgane <morganedassonville08@gmail.com>
2025-01-23 10:13:55 +0100

    add carrousel files + add style and data on the page

--- ProjetICO/src/Controller/RulesController.php
+++ ProjetICO/src/Controller/RulesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\RulesRepository;
 use App\Repository\CardsRepository;
+use App\Repository\PacksRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class RulesController extends AbstractController
 {
     #[Route('/regles', name: 'rules')]
-    public function index(RulesRepository $rulesRepository, CardsRepository $cardsRepository): Response
+    public function index(RulesRepository $rulesRepository, CardsRepository $cardsRepository, PacksRepository $packsRepository): Response
     {
         $rules = $rulesRepository->findAll();
         $cards = $cardsRepository->findAll();
+        $packs = $packsRepository->findAll();
 
         return $this->render('rules/index.html.twig', [
             'rules' => $rules,
             'cards' => $cards,
+            'packs' => $cards,
         ]);
     }
 }
