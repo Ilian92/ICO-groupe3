81140b1b7b372fcaef858e9e9110b1df332b8c1c
Ilian Igoudjil <ilianigoudjil@gmail.com>
2025-01-24 00:53:29 +0100

    Modification du fichier ico.sql pour éviter les erreurs de clés

--- ProjetICO/migrations/Version20250123235049.php
+++ ProjetICO/migrations/Version20250123235049.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250123202510 extends AbstractMigration
+final class Version20250123235049 extends AbstractMigration
 {
     public function getDescription(): string
     {
